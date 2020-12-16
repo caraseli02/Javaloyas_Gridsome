b6fb5f9df2698cd792cc45747a172b14ebbc2ea3
vlad <caraseli02@gmail.com>
2020-12-16 20:17:47 +0100

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,30 @@
 module.exports = {
-  enabled: process.env.NODE_ENV === "production",
-  content: [
-    "./src/**/*.vue",
-    "./src/**/*.js",
-    "./src/**/*.jsx",
-    "./src/**/*.html",
-    "./src/**/*.pug",
-    "./src/**/*.md",
-  ],
-  whitelist: [
-    "body",
-    "html",
-    "img",
-    "a",
-    "g-image",
-    "g-image--lazy",
-    "g-image--loaded",
-  ],
-  extractors: [
-    {
-      extractor: (content) => content.match(/[A-z0-9-:\\/]+/g),
-      extensions: ["vue", "js", "jsx", "md", "html", "pug"],
-    },
-  ],
+  purge: {
+    enabled: process.env.NODE_ENV === "production",
+    content: [
+      "./src/**/*.vue",
+      "./src/**/*.js",
+      "./src/**/*.jsx",
+      "./src/**/*.html",
+      "./src/**/*.pug",
+      "./src/**/*.md",
+    ],
+    whitelist: [
+      "body",
+      "html",
+      "img",
+      "a",
+      "g-image",
+      "g-image--lazy",
+      "g-image--loaded",
+    ],
+    extractors: [
+      {
+        extractor: (content) => content.match(/[A-z0-9-:\\/]+/g),
+        extensions: ["vue", "js", "jsx", "md", "html", "pug"],
+      },
+    ],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
